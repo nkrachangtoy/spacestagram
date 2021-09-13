d30e50b4f1a868c647bb733a912fadddf4f52035
Nusorn Krachangtoy <nkrachangtoy@gmail.com>
2021-09-13 16:33:08 -0700

    successfully fetch api

---
+++ src/API/requests.js
@@ -0,0 +1,6 @@
+const requests = {
+  // apodImages: `apod?api_key=${process.env.REACT_APP_API_KEY}`,
+  apodImages: `apod?api_key=dFNmIatD86C6JIHtTDIFFsMclshIRgZ8F0U36LWq`,
+};
+
+export default requests;
--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import axios from "./API/axios";
+import requests from "./API/requests";
 
-function App() {
+const App = () => {
+  const [images, setImages] = useState();
+
+  const fetchData = async () => {
+    const request = await axios.get(requests.apodImages);
+    setImages(request.data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Spacestagram</h1>
+        <h2>{images.title}</h2>
+        <h3>{images.date}</h3>
+        <img src={images.url} alt={images.title} />
+        <p>{images.explanation}</p>
       </header>
     </div>
   );
-}
+};
 
 export default App;
