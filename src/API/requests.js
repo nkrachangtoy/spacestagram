e0a638ee188b8c60c29d4d958cea08867408d950
Nusorn Krachangtoy <nkrachangtoy@gmail.com>
2021-09-14 11:24:43 -0700

    replace API key with env variable

--- src/API/requests.js
+++ src/API/requests.js
@@ -1,6 +1,5 @@
 const requests = {
-  // apodImages: `apod?api_key=${process.env.REACT_APP_API_KEY}`,
-  apodImages: `apod?api_key=dFNmIatD86C6JIHtTDIFFsMclshIRgZ8F0U36LWq`,
+  apodImages: `apod?api_key=${process.env.REACT_APP_API_KEY}`,
 };
 
 export default requests;
